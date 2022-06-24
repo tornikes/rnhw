133dcd2bdac1f16c96c738803d43162e0faf869b
tornikes <t.sadghobelashvili@gmail.com>
2022-06-24 15:35:52 +0400

    feat: implement TopBar and Search Input

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,14 +1,56 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 import TopBar from './TopBar';
+import MGlass from '../assets/MagnifyingGlass.svg';
 
 function MainScreen() {
+  const [searchInput, setSearchInput] = useState('');
+
   return (
     <View>
       <TopBar />
+      <View style={styles.inputContainer}>
+        <View style={styles.inputWrapper}>
+          <MGlass width={17} height={17} />
+          <TextInput
+            maxLength={50}
+            style={styles.inputStyles}
+            onChangeText={setSearchInput}
+            defaultValue={searchInput}
+          />
+        </View>
+      </View>
       <Text>This is the main screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    paddingHorizontal: 35,
+    paddingVertical: 28,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  inputWrapper: {
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderStyle: 'solid',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 5,
+    paddingLeft: 15,
+  },
+  inputStyles: {
+    marginLeft: 5,
+    flexGrow: 1,
+    overflow: 'hidden',
+  },
+});
 
 export default MainScreen;
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { SvgUri } from 'react-native-svg';
 import theme from '../theme';
+import Burger from '../assets/Burger.svg';
+import Cart from '../assets/Cart.svg';
 
 function TopBar() {
   return (
     <View style={styles.topBarStyles}>
-      <SvgUri uri="../assets/menu.svg" />
+      <Burger height="13" width="17" />
       <Text style={styles.heading}>Ecommerce Store</Text>
+      <Cart height="20" width="20" />
     </View>
   );
 }
@@ -16,6 +18,10 @@ const styles = StyleSheet.create({
   topBarStyles: {
     backgroundColor: theme.colors.primaryBg,
     paddingVertical: 16,
+    paddingHorizontal: 19,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   heading: {
     color: 'white',
